d349e3576ad143a8e1d280ac82045fd80bd1fe11
Albertina Cruz Casco <cruz.albertina@gmail.com>
2025-10-10 02:32:13 -0600

    se elimina proyecto basico

--- ProyectoReact/reactpractica/src/Componentes/comidas/comidas.jsx
+++
@@ -1,9 +0,0 @@
-function favoritos({imagen,comidaFavorita}){
-    return(
-        <div>
-            <h1>Comidas favoritos {comidaFavorita}</h1>
-            <img src={imagen}  className='imagen'></img>
-        </div>
-    );
-}
-export default favoritos;
--- ProyectoReact/reactpractica/src/Componentes/favoritos/favoritos.jsx
+++
@@ -1,9 +0,0 @@
-function favoritos({imagen,carrosFavorito}){
-    return(
-        <div>
-            <h1>Carros favoritos {carrosFavorito}</h1>
-            <img src={imagen}  className='imagen'></img>
-        </div>
-    );
-}
-export default favoritos;
